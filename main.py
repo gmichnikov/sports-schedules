634f4be9a9b6968d90f4b7b0f4089abbc78522f9
Greg Michnikov <greg.michnikov@gmail.com>
2023-12-22 08:00:16 -0500

    Add MLS and fill in NULL city/state as part of combining CSVs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-import nba, ncaab, nhl, nfl, mlb, ncaah, milb
+import nba, ncaab, nhl, nfl, mlb, ncaah, milb, mls
 import os
 from datetime import datetime
 import pandas as pd
@@ -55,6 +55,26 @@ def write_to_csv(sport, games, location_lookup):
                     writer.writerow(headers)
                 writer.writerow(game)  # Write the entire game array as it is
 
+    elif sport == 'mls':
+        for game in games:
+            game_date = game[3]  # Get the date field from the game array
+            folder = os.path.join(base_folder, game_date)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            filename = f'{game_date}_schedule_{current_datetime}.csv'
+            filepath = os.path.join(folder, filename)
+
+            with open(filepath, 'a', newline='') as file:
+                writer = csv.writer(file)
+                if os.path.getsize(filepath) == 0:  # Write headers only if file is empty
+                    writer.writerow(headers)
+                league = game[2].lower()
+                home_team = game[6].lower()
+                print("debug" + league + " " + home_team)
+                city, state = location_lookup.get((league, home_team), ("", ""))
+                writer.writerow(game + [city, state])
+
     # For other sports, write all games to a single file
     else:
         filename = f'{sport}_schedule_{current_datetime}.csv'
@@ -78,6 +98,7 @@ def create_location_lookup():
             key = (row[0].lower(), row[1].lower())  # League and Team Name
             value = (row[2], row[3])  # City and State
             location_lookup[key] = value
+    # print(str(location_lookup))
     return location_lookup
 
 def get_most_recent_csv(folder):
@@ -86,7 +107,7 @@ def get_most_recent_csv(folder):
     most_recent_file = sorted(csv_files, key=lambda x: os.path.getctime(os.path.join(folder, x)), reverse=True)[0]
     return os.path.join(folder, most_recent_file)
 
-def combine_csv_files(delete_dupes):
+def combine_csv_files(delete_dupes, location_lookup):
     schedule_folders = [f for f in os.listdir('.') if os.path.isdir(f) and f.endswith('_schedule')]
     combined_folder = 'combined_schedules'
     if not os.path.exists(combined_folder):
@@ -114,12 +135,24 @@ def combine_csv_files(delete_dupes):
     column_order = ['primary_key'] + [col for col in combined_df.columns if col != 'primary_key']
     combined_df = combined_df[column_order]
 
+    for index, row in combined_df.iterrows():
+        if pd.isna(row['home_city']) or row['home_city'] == '':
+            lookup_key = (row['league'].lower(), row['home_team'].lower())
+            if lookup_key in location_lookup:
+                combined_df.at[index, 'home_city'] = location_lookup[lookup_key][0]
+
+        if pd.isna(row['home_state']) or row['home_state'] == '':
+            lookup_key = (row['league'].lower(), row['home_team'].lower())
+            if lookup_key in location_lookup:
+                combined_df.at[index, 'home_state'] = location_lookup[lookup_key][1]
+
+
     current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
     combined_filename = f'combined_schedule_{current_datetime}.csv'
     combined_df.to_csv(os.path.join(combined_folder, combined_filename), index=False)
 
 def add_missing_teams_to_locations(combined_df):
-    # filtered_df = combined_df[combined_df['sport'] != 'milb']
+    # milb does not use location lookup
     filtered_df = combined_df[~((combined_df['sport'] == 'baseball') & (combined_df['level'] == 'minors'))]
     locations_df = pd.read_csv('locations.csv')
 
@@ -150,7 +183,7 @@ def create_combined_df(folders):
             for team_folder in team_folders:
                 csv_file = get_most_recent_csv(team_folder)
                 if csv_file:
-                    print("combining csv: " + str(csv_file))
+                    # print("combining csv: " + str(csv_file))
                     df = pd.read_csv(csv_file, header=0)
                     combined_df = pd.concat([combined_df, df], ignore_index=True)
         elif 'milb' in folder:
@@ -161,10 +194,18 @@ def create_combined_df(folders):
                     # print("combining csv: " + str(csv_file))
                     df = pd.read_csv(csv_file, header=0)
                     combined_df = pd.concat([combined_df, df], ignore_index=True)
+        elif 'mls' in folder:
+            date_folders = [os.path.join(folder, d) for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+            for date_folder in date_folders:
+                csv_file = get_most_recent_csv(date_folder)
+                if csv_file:
+                    # print("combining csv: " + str(csv_file))
+                    df = pd.read_csv(csv_file, header=0)
+                    combined_df = pd.concat([combined_df, df], ignore_index=True)
         else:
             csv_file = get_most_recent_csv(folder)
             if csv_file:
-                print("combining csv: " + str(csv_file))
+                # print("combining csv: " + str(csv_file))
                 df = pd.read_csv(csv_file, header=0)
                 combined_df = pd.concat([combined_df, df], ignore_index=True)
 
@@ -182,6 +223,7 @@ def main(delete_dupes):
         # 'nfl': nfl,
         # 'mlb': mlb,
         # 'milb': milb,
+        # 'mls': mls,
     }
 
     for sport, module in sports_modules.items():
@@ -189,7 +231,7 @@ def main(delete_dupes):
         games = module.scrape_sites()
         write_to_csv(sport, games, location_lookup)
 
-    combine_csv_files(delete_dupes)
+    combine_csv_files(delete_dupes, location_lookup)
 
 if __name__ == "__main__":
     main(args.delete_dupes)
---
+++ mls.py
@@ -0,0 +1,81 @@
+from bs4 import BeautifulSoup
+import json
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def load_urls():
+    with open('inputs.json', 'r') as file:
+        data = json.load(file)
+        return data
+
+def parse_game_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d')
+
+def scrape_sites():
+    games = []
+    json_content = load_urls()
+    site = json_content["mls"]['sites'][0]
+    start_date = parse_game_date(json_content["mls"]['start_date'])
+    end_date = parse_game_date(json_content["mls"]['end_date'])
+
+    # Set up Selenium with headless options
+    options = Options()
+    options.headless = True
+    service = Service(ChromeDriverManager().install())
+
+    # Iterate over dates
+    current_date = start_date
+    while current_date <= end_date:
+        driver = webdriver.Chrome(service=service, options=options)
+
+        schedule_date = current_date.strftime('%Y-%m-%d')
+
+        # Use Selenium to get the page content after JavaScript execution
+        driver.get(site + schedule_date)
+
+        # Wait for the necessary time for the page to load and JavaScript to execute
+        time.sleep(5)
+
+        # Get the page source and close the browser
+        page_source = driver.page_source
+
+        # You can continue with BeautifulSoup to parse the page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        # Find all games for the day
+        game_containers = soup.find_all('div', class_='mls-c-match-list__match-container')
+
+        for game in game_containers:
+            try:
+                date_div = game.find('div', {'class': 'mls-c-status-stamp__status'})
+                date_str = date_div.get_text().strip() if date_div else ''
+
+                # Assume year 2024 and parse the date
+                game_date = datetime.strptime(date_str + ' 2024', '%m/%d %Y')
+                formatted_date = game_date.strftime('%Y-%m-%d')
+                day_of_week = game_date.strftime('%A')
+
+                away_team_div = game.find('div', class_='--away')
+                away_team = away_team_div.find('span', class_='mls-c-club__shortname').get_text().strip()
+
+                home_team_div = game.find('div', class_='--home')
+                home_team = home_team_div.find('span', class_='mls-c-club__shortname').get_text().strip()
+
+                gametime = game.find('div', class_='mls-c-scorebug').find('span').get_text().strip()
+
+                venue = game.find('p', class_='sc-kgTSHT').get_text().strip()
+
+                game_data = ['soccer', 'pro', 'mls', formatted_date, day_of_week, gametime, home_team, away_team, venue]
+                games.append(game_data)
+            except Exception as e:
+                print(f"Error processing game: {e}")
+
+
+        current_date += timedelta(days=7)
+        driver.quit()
+    
+    return games
